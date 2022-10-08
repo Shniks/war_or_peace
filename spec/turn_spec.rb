bda84fd3342744864881c46a32486296ce470f5f
Nikhil Shahi <shniks@gmail.com>
2022-10-08 19:38:51 +0530

    Created test for a war type of turn.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -71,4 +71,14 @@ RSpec.describe Turn do
     expect(@player2.deck.cards).to eq([@card4, @card6, @card7])
   end
 
+  it 'displays a war type of turn' do
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    expect(turn.type).to eq(:war)
+  end
+  
 end
