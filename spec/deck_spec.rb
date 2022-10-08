e40d570c8b85fcc3e0b1dd2e55193d1d3fa494d4
Nikhil Shahi <shniks@gmail.com>
2022-10-08 16:35:35 +0530

    Created test to check for attributes of deck class.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require './lib/deck'
+require 'pry'
 
 RSpec.describe Deck do
   before do
@@ -7,12 +8,20 @@ RSpec.describe Deck do
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     @cards = [card1, card2, card3]
-  end 
+  end
 
   it 'exists' do
     deck = Deck.new(@cards)
 
     expect(deck).to be_a(Deck)
+  end
+
+  it 'has attributes' do
+    deck = Deck.new(@cards)
+
+    expect(deck.cards.count).to eq(3)
+    expect(deck.cards).to be_a(Array)
+    expect(deck.cards).to eq(@cards)
   end
 
 end
