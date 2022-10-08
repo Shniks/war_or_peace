ae7c8384b777e66b25e6bb3a964a2584dedf0903
Nikhil Shahi <shniks@gmail.com>
2022-10-08 18:05:19 +0530

    Created methods to determine type of turn. Carved out the types into separate methods.

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,22 @@ class Turn
     @spoils_of_war = []
   end
 
+  def type
+    return :basic if basic == true
+    return :war if war == true
+    return :mutually_assured_destruction if war == true && second_index == true
+  end 
+
+  def basic
+    player1.deck.rank_of_card(0) != player2.deck.rank_of_card(0)
+  end
+
+  def war
+    player1.deck.rank_of_card(0) == player2.deck.rank_of_card(0)
+  end
+
+  def second_index
+    player1.deck.rank_of_card(2) == player2.deck.rank_of_card(2)
+  end
+
 end
